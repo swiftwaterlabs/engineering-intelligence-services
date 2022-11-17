8cb7734dacb8c668bd4b6137cd1ca075052a910d
Josh Rolstad <jrolstad@salesforce.com>
2022-11-16 21:15:29 -0800

    Adding query for test results

--- analysis/queries/test_results.sql
+++ analysis/queries/test_results.sql
@@ -1,7 +1,13 @@
-select id as Id,
-       project as Project,
-       split(project,'.')[1] as Organization,
-       split(project,'.')[2] as Repository,
-       from_iso8601_timestamp(analyzedat) as AnalyzedAt,
-       cast(metrics.coverage as decimal(10,4)) as CodeCoverage
-from engineering_intelligence_prd.test_result
+select distinct tr.id as Id,
+                tr.project as Project,
+                element_at(split(tr.project,'.'),1) as Organization,
+                element_at(split(tr.project,'.'),2) as Repository,
+                from_iso8601_timestamp(tr.analyzedat) as AnalyzedAt,
+                cast(tr.metrics.coverage as decimal(10,4)) as CodeCoverage,
+                cast(tr.metrics.lines_to_cover as int) as TotalLines,
+                cast(tr.metrics.uncovered_lines as int) as UnCoveredLines,
+                ro.parentowner as ParentOwner
+from engineering_intelligence_prd.test_result tr
+         left join engineering_intelligence_prd.repository_owner ro on element_at(split(tr.project,'.'),1) = ro.repository.organization.name AND
+                                                                       element_at(split(tr.project,'.'),2) = ro.repository.name
+order by tr.project,from_iso8601_timestamp(tr.analyzedat)
