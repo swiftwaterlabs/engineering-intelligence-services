55f7d594f3fc54da70c9bf3975d4c1d074785b43
Josh Rolstad <jrolstad@salesforce.com>
2022-11-16 20:20:58 -0800

    Adding test result table

--- src/deployments/infra/glue_database.tf
+++ src/deployments/infra/glue_database.tf
@@ -343,3 +343,51 @@ resource "aws_glue_catalog_table" "webhook" {
 
   }
 }
+
+resource "aws_glue_catalog_table" "test_result" {
+  name          = "test_result"
+  database_name = aws_glue_catalog_database.object_db.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${local.signal_bucket_name}/testresult"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json"
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
+    columns {
+      name = "id"
+      type = "string"
+    }
+
+    columns {
+      name = "project"
+      type = "string"
+    }
+
+    columns {
+      name = "analyzedat"
+      type = "string"
+    }
+
+    columns{
+      name = "metrics"
+      type = "struct<coverage:string,lines_to_cover:string,uncovered_lines:string>"
+    }
+
+
+  }
+}
